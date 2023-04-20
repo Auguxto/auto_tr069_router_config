0f7e909385b2b320a46697907c8bdff2d322d906
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2023-04-20 13:46:56 -0300

    add: environment variables

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -3,6 +3,10 @@ declare global {
     interface ProcessEnv {
       ROUTER_PORT: number;
       DEFAULT_ROUTER_PASSWORD: string;
+      ACS_URL: string;
+      ACS_INFORM_INTERVAL: number;
+      ACS_USER?: string;
+      ACS_PASSWORD?: string;
     }
   }
 }
